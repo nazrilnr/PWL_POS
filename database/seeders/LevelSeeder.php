9f09afa41048cf623288a1537102c11d20b8550d
MuhammadNazrilNurRahman <mnazrilnurrahman@gmail.com>
2025-03-06 09:54:56 +0700

    pratikum 6

--- database/seeders/LevelSeeder.php
+++ database/seeders/LevelSeeder.php
@@ -17,8 +17,13 @@ class LevelSeeder extends Seeder
             ['level_id' => 1, 'level_kode' => 'ADM', 'level_nama' => 'Administrator'],
             ['level_id' => 2, 'level_kode' => 'MNG', 'level_nama' => 'Manager'],
             ['level_id' => 3, 'level_kode' => 'STF', 'level_nama' => 'Staff/Kasir'],
+            ['level_id' => 4, 'level_kode' => 'PLG', 'level_nama' => 'Pelanggan'],
         ];
 
-        DB::table('m_level')->insert($data);
-    }
+        foreach ($data as $level) {
+            DB::table('m_level')->updateOrInsert(
+                ['level_id' => $level['level_id']], // Kondisi
+                $level // Data yang akan diperbarui atau disisipkan
+            );    }
+        }
 }
