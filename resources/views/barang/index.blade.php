d7a9f1856e325defbf70581ad2f67f3a3df9ee66
MuhammadNazrilNurRahman <mnazrilnurrahman@gmail.com>
2025-05-20 18:02:49 +0700

    Export PDF

--- resources/views/barang/index.blade.php
+++ resources/views/barang/index.blade.php
@@ -7,6 +7,9 @@
         <div class="card-tools">
             <button onclick="modalAction('{{ url('/barang/import') }}')" class="btn btn-info">Import Barang</button>
             <a href="{{ url('/barang/export_excel') }}" class="btn btn-primary"><i class="fa fa-file- excel"></i> Export Barang</a>
+            <a href="{{ url('/barang/export_pdf') }}" class="btn btn-danger"><i class="fa fa-file-pdf-o"></i> Export PDF
+</a>
+
             <button onclick="modalAction('{{ url('/barang/create_ajax') }}')" class="btn btn-success">Tambah Data (Ajax)</button>
         </div>
     </div>
